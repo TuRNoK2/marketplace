0d4642dbcaf76ca9075f9b378ceecf0a0b7cbb41
TuRNoK2 <turnok2@example.com>
2025-04-20 18:43:26 +0500

    html1

--- marketplace/urls.py
+++ marketplace/urls.py
@@ -16,12 +16,17 @@ Including another URLconf
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
+from django.shortcuts import render
 from django.urls import path, include
+
+def home_view(request):
+    return render(request, 'home.html')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("users/", include('users.urls')),
-    path('', include('products.urls'))
+    path('', include('products.urls')),
+    path('', home_view, name='home'),
 ]
 
 if settings.DEBUG:
