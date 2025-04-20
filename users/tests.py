ec8cb27799c40a825c365d3f1383b34041d79015
TuRNoK2 <turnok2@example.com>
2025-04-20 16:55:02 +0500

    commit2

--- users/tests.py
+++ users/tests.py
@@ -1,3 +1,29 @@
 from django.test import TestCase
+from django.contrib.auth import get_user_model
 
-# Create your tests here.
+User = get_user_model()
+
+# test проверка создания пользователя
+class UserTest(TestCase):
+    def test_create_and_list_users(self):
+
+        User.objects.create_user(
+            username='testuser',
+            email='test@example.com',
+            password='123'
+        )
+
+        User.objects.create_user(
+            username='anotheruser',
+            email='another@example.com',
+            password='456'
+        )
+
+        users = User.objects.all()
+        print("\n📋 Список пользователей:")
+        for user in users:
+            print(f"👤 Username: {user.username}, Email: {user.email}")
+
+
+        self.assertEqual(users.count(), 2)  # проверка
+
